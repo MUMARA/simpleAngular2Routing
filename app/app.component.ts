a2b85855b179e4440d238a13d1c552d41eee3a53
umar <muhammadumar02@gmail.com>
2017-01-16 21:49:33 +0500

    simple is complete

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1> 
- <my-heroes></my-heroes>
+
  <nav> <a routerLink="/heroes">Heroes</a></nav>
    <router-outlet></router-outlet>
 `
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent}  from './app.component';
 import {HeroesComponent}     from './heroes.component';
+import { RouterModule, Routes } from '@angular/router';
 
 
 @NgModule({
